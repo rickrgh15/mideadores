f9cc1cf65dc16c6daa814614a7724e75c68bf2ff
Ricardo González <rgonzalezh1400@alumno.ipn.mx>
2023-03-04 14:22:42 -0600

    adding login complete c:

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,21 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDb7TjhI46seWGFliwbrZ-5dlKtvJIvB4o",
-  authDomain: "mideadores-4298a.firebaseapp.com",
-  databaseURL: "https://mideadores-4298a-default-rtdb.firebaseio.com",
-  projectId: "mideadores-4298a",
-  storageBucket: "mideadores-4298a.appspot.com",
-  messagingSenderId: "527995253244",
-  appId: "1:527995253244:web:b3ee00c3c98399ff7c0279",
-  measurementId: "G-3Y265NFSNH"
-};
+var firebaseConfig = {
+    apiKey: "AIzaSyDb7TjhI46seWGFliwbrZ-5dlKtvJIvB4o",
+    authDomain: "mideadores-4298a.firebaseapp.com",
+    databaseURL: "https://mideadores-4298a-default-rtdb.firebaseio.com",
+    projectId: "mideadores-4298a",
+    storageBucket: "mideadores-4298a.appspot.com",
+    messagingSenderId: "527995253244",
+    appId: "1:527995253244:web:b3ee00c3c98399ff7c0279",
+    measurementId: "G-3Y265NFSNH"
+  };
 
-// init firebase
-firebase.initializaApp(firebaseConfig)
 
-// init services
-const projectFirestore = firebase.firestore()
+  const app = initializeApp(firebaseConfig);
 
-export {projectFirestore}
+  export const auth = getAuth();
+  export default app;
